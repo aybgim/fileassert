180d72885c325aeee8dc5d6da30aeda35a7f2df0
Albert Gevorgyan <ablertus@yahoo.com>
2023-07-08 14:45:31 +0100

    API documentation.

--- src/main/java/org/aybgim/fileassert/FileAssert.java
+++ src/main/java/org/aybgim/fileassert/FileAssert.java
@@ -18,16 +18,61 @@ import org.junit.jupiter.api.TestInfo;
 
 import java.util.function.Function;
 
+/**
+ * <p>This class is used for asserting the correctness of an object's string representation by comparing it
+ * to the content of a file in src/test/resources.</p>
+ *
+ * <p>The relative path to the file is defined as
+ * src/test/resources/{testClassPath}/{testClassName}/{testName}.{ext}
+ * where {testClassPath} is the path to the location of resources for the test class
+ * (derived from package name by replacing dots with separators),
+ * {testClassName} is the name of the test class,
+ * {testName} is the name of the unit test, and {ext} is a pre-defined file extension.
+ * <br>
+ * For example, for the unit test method {@link org.aybgim.fileassert.FileAssertTest#testAssertTextFile}, and
+ * the fileExtension ".txt", the path should be
+ * {@code src/test/resources/org/aybgim/fileassert/FileAssertTest/testAssertTextFile.txt}
+ * </p>
+ *
+ * <p>The string representation function depends on the implementation: it can be simple {@code Object::toString}, or
+ * a serialization method, e.g. in JSON format. </p>
+ *
+ * <p>Depending on the implementation, this class can behave in two different modes: testing or writing, see
+ * {@link #assertWithFile(Object, TestInfo)}. Hence, this class can be used both for testing against files, and
+ * for maintaining them.</p>
+ */
 public class FileAssert {
     private final String fileExtension;
     private final Function<Object, String> stringRepresentation;
     private final TestFileProcessor fileProcessor;
 
+    /**
+     * Construct a FileAssert object.
+     * @param fileExtension file extension, e.g. "txt" or "json"
+     * @param stringRepresentation string representation function, e.g. Object::String
+     * @param fileProcessor {@link TestFileProcessor} which defines the behaviour mode - testing or writing.
+     */
     protected FileAssert(String fileExtension, Function<Object, String> stringRepresentation, TestFileProcessor fileProcessor) {
         this.fileExtension = fileExtension;
         this.stringRepresentation = stringRepresentation;
         this.fileProcessor = fileProcessor;
     }
+
+    /**
+     * <p>Depending on the implementation, this method may behave in two different modes:
+     * <ul>
+     *     <li>Testing mode: this is the default mode, when the string representation of the object is matched to
+     *     the content of the file. The matching method and the behaviour following mismatch depend on the
+     *     implementation - by default, this would be simple string equality and an AssertionError.</li>
+     *     <li>Writing mode: in this mode, the file is created in the expected directory (see above), and the
+     *     string representation of the object is written to the file. This mode is used to create files for
+     *     new tests, or to overwrite them, when the expected string representation changes.</li>
+     * </ul>
+     * </p>
+     * @param actual object whose string representation is tested
+     * @param info test information used to obtain test class and test method name.
+     * @throws Exception
+     */
     public void assertWithFile(Object actual, TestInfo info) throws Exception {
         Class<?> testClass = info.getTestClass()
                 .orElseThrow();
--- src/main/java/org/aybgim/fileassert/FileAsserts.java
+++ src/main/java/org/aybgim/fileassert/FileAsserts.java
@@ -18,17 +18,50 @@ import org.junit.jupiter.api.Assertions;
 
 import java.util.function.Function;
 
+/**
+ * Utility class providing factory methods for {@link FileAssert}.
+ */
 public class FileAsserts {
+
+    /**
+     * System property defining whether the resulting {@link FileAssert} implementation
+     * returned by a factory method is testing or writing. See {@link #fileAssert(String, TextAssertion, Function)}
+     */
     public static final String FILEASSERT_GENERATE_PROP = "fileassert.generate";
     private static final WritingTestFileProcessor WRITING_TEST_FILE_PROCESSOR = new WritingTestFileProcessor();
+
+    /**
+     * Equivalent to calling {@link #fileAssert(String, TextAssertion)} with
+     * {@code Assertions::assertEquals} as the second parameter.
+     * @param fileExtension file extension, e.g. "txt" or "json"
+     * @return {@link FileAssert FileAssert object}
+     */
     public static FileAssert fileAssert(String fileExtension) {
         return fileAssert(fileExtension, Assertions::assertEquals);
     }
 
+    /**
+     * Equivalent to calling {@link #fileAssert(String, TextAssertion, Function)} with
+     * {@code Object::toString} as the third parameter.
+     * @param fileExtension file extension, e.g. "txt" or "json"
+     * @param textAssertion assertion method, see {@link #fileAssert(String, TextAssertion, Function)} documentation.
+     * @return {@link FileAssert FileAssert object}
+     */
     public static FileAssert fileAssert(String fileExtension, TextAssertion textAssertion) {
         return fileAssert(fileExtension, textAssertion, Object::toString);
     }
 
+    /**
+     * Get a {@link FileAssert}. If the {@value FILEASSERT_GENERATE_PROP} system property
+     * is "true", the returned FileAssert is writing, otherwise it is testing.
+     *
+     * @param fileExtension file extension, e.g. "txt" or "json"
+     * @param textAssertion assertion method which asserts the correctness of the string representations
+     *                      of objects by matching them to the contents of test files, and can throw an error if
+     *                      the assertion fails.
+     * @param stringRepresentation string representation function
+     * @return {@link FileAssert FileAssert object}
+     */
     public static FileAssert fileAssert(String fileExtension, TextAssertion textAssertion,
                                         Function<Object, String> stringRepresentation) {
         TestFileProcessor fileProcessor = Boolean.getBoolean(FILEASSERT_GENERATE_PROP)
